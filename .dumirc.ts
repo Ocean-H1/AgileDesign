4b20ac794bd3ccb67b159ccf517f3bee19c6c1d2
OceanH <1404939185@qq.com>
2024-11-17 05:23:01 +0800

    docs(alert): alert-doc

--- .dumirc.ts
+++ .dumirc.ts
@@ -7,14 +7,14 @@ export default defineConfig({
     name: 'AgileUI',
     // logo: '/logo.png',
     socialLinks: {
-      github: 'https://github.com/Ocean-H1/AgileUI'
+      github: 'https://github.com/Ocean-H1/AgileUI',
     },
     nav: [
       {
         title: '组件',
         order: 2,
         link: '/components/overview',
-      }
+      },
     ],
   },
   // 修改dumi默认主题的主色, 更多变量详见：https://github.com/umijs/dumi/blob/master/src/client/theme-default/styles/variables.less
--- src/alert/index.md
+++ src/alert/index.md
@@ -8,7 +8,14 @@ group:
 
 警告提示，展现需要关注的信息。
 
-## 代码演示
-
-### 基本用法
+## 基本用法
 <code src='./demo/basic.tsx'></code>
+
+## API
+
+| 属性 | 说明     | 类型                                         | 默认值 |
+| ---- | -------- | -------------------------------------------- | ------ |
+| type | 类型 | `info`\|`error`\/`success`\/`warning` | `info` |
+| message | 警告信息 | `ReactNode` | - |
+| size | 尺寸 | `small` \| `medium` \| `large` | `medium` |
+| description | 描述信息 | `ReactNode`| - |
