b3924104fb0fc7af44a15e18bb34104a5935adc1
OceanH <1404939185@qq.com>
2024-11-25 12:43:05 +0800

    feat(icon): 修改IconProps

--- src/icon/index.tsx
+++ src/icon/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { Icon as IfyIcon } from '@iconify/react';
+import { Icon as IfyIcon, IconProps as IfyIconProps } from '@iconify/react';
 
-export interface IconProps {
-  icon: string;
-}
+export interface IconProps extends IfyIconProps {}
 
 /**
  * 暂时使用 Iconify(https://iconify.design/), 后面如有时间精力可考虑替换私有图标库
- * @param icon 图标名称 
- * @returns 
  */
 const Icon: React.FC<IconProps> = ({ icon, ...props }) => {
   return <IfyIcon icon={icon} {...props}></IfyIcon>;
