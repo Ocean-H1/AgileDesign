e7955b878714c6a56e4a6e84c48df447b39fe876
OceanH <1404939185@qq.com>
2024-12-14 00:00:13 +0800

    build(gulp): 处理样式文件, 生成css.js

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -3,6 +3,7 @@ import babel from 'gulp-babel';
 import less from "gulp-less";
 import cleanCss from 'gulp-clean-css';
 import autoPrefixer from 'gulp-autoprefixer';
+import through2 from 'through2';
 
 const paths = {
   dest: {
@@ -21,6 +22,23 @@ function compileScripts(babelEnv, destDir) {
   return gulp
     .src(scripts)
     .pipe(babel())
+    .pipe(
+      through2.obj(function (file, encoding, next) {
+        try {
+          this.push(file.clone());
+
+          if (file.path.match(/(\/|\\)style(\/|\\)index\.js/)) {
+            const content = file.contents.toString(encoding);
+            file.contents = Buffer.from(cssInjection(content));
+            file.path = file.path.replace(/index\.js/, 'css.js');
+            this.push(file);
+          }
+          next();
+        } catch (error) {
+          next(error);
+        }
+      })
+    )
     .pipe(gulp.dest(destDir));
 }
 
@@ -61,6 +79,27 @@ function less2css() {
     }))
     .pipe(gulp.dest(paths.dest.lib))
     .pipe(gulp.dest(paths.dest.esm));
+}
+
+/**
+ * 当前组件样式 import './index.less' => import './index.css'
+ * 依赖的其他组件样式 import '../test-comp/style' => import '../test-comp/style/css.js'
+ * 依赖的其他组件样式 import '../test-comp/style/index.js' => import '../test-comp/style/css.js'
+ * @param {string} content
+ */
+function cssInjection(content) {
+  return content.replace(/import\s+(['"])([^'"]*\.less)\1/g, (match, quote, path) => {
+    // 将 .less 替换为 .css
+    const newPath = path.replace('.less', '.css');
+    
+    // 如果路径包含 /style 或 /style/index.js，则进一步替换为 /style/css.js
+    if (newPath.endsWith('/style') || newPath.endsWith('/style/index.js')) {
+      return `import ${quote}${newPath.replace(/(\/style)(?:\/index\.js)?$/, '$1/css.js')}${quote}`;
+    }
+
+    // 否则，直接返回替换后的 .css 路径
+    return `import ${quote}${newPath}${quote}`;
+  });
 }
 
 // 串行编译脚本，避免环境变量影响
